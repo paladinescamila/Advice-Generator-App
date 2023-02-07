4a6e79ee55193531ca91c925c3cf923cd654e7a6
Camila Paladines <camilapaladines27@gmail.com>
2023-02-07 17:03:28 -0500

    Add components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import IconDice from './assets/icon-dice.svg';
+import {ThreeDots} from 'react-loader-spinner';
+import DividerIcon from './assets/divider-icon.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [adviceNumber, setAdviceNumber] = useState<number>(117);
+	const [adviceText, setAdviceText] = useState<string>('It is easy to sit up and take notice, what’s difficult is getting up and taking action.');
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const loadAdvice = () => {
+		setLoading(true);
+
+		fetch('https://api.adviceslip.com/advice').then((response) => {
+			response.json().then((data) => {
+				setAdviceNumber(data.slip.id);
+				setAdviceText(data.slip.advice);
+				setLoading(false);
+			});
+		});
+	};
+
+	return (
+		<div className='app'>
+			<div className='card'>
+				<p className='advice-number'>ADVICE #{adviceNumber}</p>
+				<p className='advice-text'>"{adviceText}"</p>
+				<div className='divider-container'>
+					<img src={DividerIcon} alt='' className='divider-icon' />
+				</div>
+				<button className='icon-dice-button' onClick={loadAdvice}>
+					<ThreeDots height='30' width='30' radius='9' color='hsl(218, 23%, 16%)' visible={loading} />
+					{!loading && <img src={IconDice} alt='' className='icon-dice-img' />}
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
